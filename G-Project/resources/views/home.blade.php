1d94fbbe080e710036445db9adfb36fe111db863
Wes021 <122152471+Wes021@users.noreply.github.com>
2024-10-21 18:23:26 +0300

    s

--- G-Project/resources/views/home.blade.php
+++ G-Project/resources/views/home.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>home</title>
 </head>
 <body>
     <div>
-        <h1>يارب تزبط</h1>
+        <h1>hi</h1>
     </div>
 </body>
 </html>
