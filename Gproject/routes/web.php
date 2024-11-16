3894f1bfd3cc29e574d3e0df90c0ec6de28f5ff4
Wes021 <122152471+Wes021@users.noreply.github.com>
2024-11-16 11:22:58 +0300

    code cleaning

--- Gproject/routes/web.php
+++ Gproject/routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
-use App\Models\CategoryApp;
-use App\Models\Customer;
 
 /////////CustomerController/////////
 
